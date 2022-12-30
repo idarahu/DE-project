dc6ed3eeb64b9e78f98c3d8a3554308a20476c6e
Siim Karel Koger <siimkkoger@gmail.com>
2022-12-30 17:40:01 +0200

    Update queries_ranking_authors.sql
    couple fixes

--- queries_ranking_authors.sql
+++ queries_ranking_authors.sql
@@ -99,9 +99,9 @@ FROM
 	JOIN warehouse.authors aut
 	JOIN warehouse.publication_author pub_auth
 	JOIN warehouse.publication_time pub_time
-	ON pub_auth.publication_id = pub.id
+	ON pub_auth.publication_id = pub.pub_id
 	AND pub_auth.author_id = aut.id
-	AND pub.id = pub_time.publication_id
+	AND pub.pub_id = pub_time.publication_id
 WHERE 
 	DATE_PART('year', pub_time.date::date) = 'year_val'
 GROUP BY aut.id
@@ -124,7 +124,7 @@ FROM
 	JOIN warehouse.publication_author pub_auth
 	JOIN warehouse.publication_domain pub_domain
 	JOIN warehouse.scientific_domain scientific_domain
-	ON pub_auth.publication_id = pub.id
+	ON pub_auth.publication_id = pub.pub_id
 	AND pub_auth.author_id = aut.id
 	AND pub_domain.domain_id = scientific_domain.id
 WHERE scientific_domain.id = 'scientific_domain_id'
@@ -146,9 +146,9 @@ FROM
 	JOIN warehouse.authors aut
 	JOIN warehouse.publication_author pub_auth
 	JOIN warehouse.publication_venues pub_venues
-	ON pub_auth.publication_id = pub.id
+	ON pub_auth.publication_id = pub.pub_id
 	AND pub_auth.author_id = aut.id
-	AND pub_venues.publication_id = pub.id
+	AND pub_venues.publication_id = pub.pub_id
 WHERE pub_venues.id = 'pub_venues_id'
 GROUP BY aut.id
 ORDER BY number_of_citations DESC;
@@ -171,12 +171,12 @@ FROM
 	JOIN warehouse.publication_domain pub_domain
 	JOIN warehouse.scientific_domain scientific_domain
 	JOIN warehouse.publication_venues pub_venues
-	ON pub_auth.publication_id = pub.id
+	ON pub_auth.publication_id = pub.pub_id
 	AND pub_auth.author_id = aut.id
-	AND pub.id = pub_time.publication_id
-	AND pub_domain.publication_id = pub.id
+	AND pub.pub_id = pub_time.publication_id
+	AND pub_domain.publication_id = pub.pub_id
 	AND pub_domain.domain_id = scientific_domain.id
-	AND pub_venues.publication_id = pub.id
+	AND pub_venues.publication_id = pub.pub_id
 WHERE
 	DATE_PART('year', pub_time.date::date) = 'year_val'
 	AND scientific_domain.id = 'scientific_domain_id'
