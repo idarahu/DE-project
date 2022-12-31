e50c7876b6a6e0f83bcc3bdda6c14037d4f9509a
Siim Karel Koger <siimkkoger@gmail.com>
2022-12-31 15:42:38 +0200

    Update venue

--- queries_dw/queries_update.sql
+++ queries_dw/queries_update.sql
@@ -1,19 +1,31 @@
--- Update venue
+-- Update venue (if fact table had dim foreign keys)
 BEGIN;
 UPDATE warehouse.publication_venues venue
 SET
-	valid_to = now()::date;
-WHERE full_name = 'full_name';
+	venue.valid_to = now()::date;
+WHERE venue.full_name = 'full_name' AND venue.valid_to IS NULL;
 
 
 INSERT INTO warehouse.publication_venues(
-    publication_id,
     full_name,
     abbreviation,
     type,
     h_index_calculated,
     valid_from
 )
-VALUES ('publication_id', 'full_name', 'abbreviation', 'type',
+VALUES ('full_name', 'abbreviation', 'type',
 		'h_index_calculated', now()::date);
+
+UPDATE warehouse.publications pub
+SET
+	pub.venue_id = (
+		SELECT venue.id FROM warehouse.publication_venues venue
+		WHERE venue.full_name = 'full_name' AND venue.valid_to IS NULL
+		LIMIT 1
+	)
+WHERE
+	pub.venue_id IN (
+		SELECT venue.id FROM warehouse.publication_venues
+		WHERE venue.full_name = 'full_name'
+	)
 COMMIT;
