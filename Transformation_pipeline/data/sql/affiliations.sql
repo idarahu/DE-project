530b222c18b2a8510ad237036f4edead8311054c
idarahu <102286655+idarahu@users.noreply.github.com>
2023-01-07 01:36:31 +0200

    Delete affiliations.sql

--- Transformation_pipeline/data/sql/affiliations.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS affiliations (
-affiliation_ID serial PRIMARY KEY,
-institution_name VARCHAR(255),
-institution_place VARCHAR(255),
-UNIQUE (institution_name, institution_place));
