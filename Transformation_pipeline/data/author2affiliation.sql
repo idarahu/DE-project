889833c3c4788f433712b1b6b9ed02e7b6d492fa
idarahu <102286655+idarahu@users.noreply.github.com>
2023-01-07 01:31:46 +0200

    Delete Transformation_pipeline/data directory

--- Transformation_pipeline/data/author2affiliation.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS author2affiliation (
-author2affiliation_ID serial PRIMARY KEY,
-author_ID INT NOT NULL,
-affiliation_ID INT NOT NULL,
-UNIQUE (author_ID, affiliation_ID));
--- Transformation_pipeline/data/connect_author2aff.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO author2affiliation (author_id, affiliation_id)
-SELECT DISTINCT t2.author_id, t3.affiliation_id
-FROM affiliations_temp t1
-JOIN authors t2 ON t1.author_last_name = t2.last_name AND t1.author_first_name_abbr = t2.first_name_abbr
-JOIN affiliations t3 ON t1.institution_name = t3.institution_name AND t1.institution_place = t3.institution_place;
