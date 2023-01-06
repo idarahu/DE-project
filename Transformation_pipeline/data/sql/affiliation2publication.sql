0390ad3d1f15e3fa4a3bca1f5b9668aa1083c30d
idarahu <102286655+idarahu@users.noreply.github.com>
2023-01-07 01:36:23 +0200

    Delete affiliation2publication.sql

--- Transformation_pipeline/data/sql/affiliation2publication.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS affiliation2publication (
-affiliation2pub_ID serial PRIMARY KEY,
-affiliation_ID INT NOT NULL,
-publication_ID INT NOT NULL,
-UNIQUE (affiliation_ID, publication_ID));
