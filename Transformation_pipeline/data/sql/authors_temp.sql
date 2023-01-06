620a467cec2d9f1221ee34b21c2a3650daa8ba0c
idarahu <102286655+idarahu@users.noreply.github.com>
2023-01-07 01:37:12 +0200

    Delete authors_temp.sql

--- Transformation_pipeline/data/sql/authors_temp.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS authors_temp (
-publication_ID INT,
-last_name VARCHAR(255) NOT NULL,
-first_name VARCHAR(255),
-first_name_abbr VARCHAR(25) NOT NULL,
-extra VARCHAR(100),
-position TEXT,
-h_index_real INT,
-updated_at TIMESTAMP WITH TIME ZONE);
