a8664c4562c33d400577766109e85c6dfd586e3e
idarahu <102286655+idarahu@users.noreply.github.com>
2023-01-13 11:11:02 +0200

    Update update_articles_in_DB.py

--- Transformation_pipeline/dags/update_articles_in_DB.py
+++ Transformation_pipeline/dags/update_articles_in_DB.py
@@ -1,10 +1,15 @@
 import datetime
+import io
 from datetime import datetime, timedelta, timezone
 
-from airflow import DAG
-from airflow.hooks.postgres_hook import PostgresHook
+from airflow import DAG 
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator 
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.sensors.external_task import ExternalTaskMarker, ExternalTaskSensor
+from airflow.utils.task_group import TaskGroup
+from airflow.operators.empty import EmptyOperator
+from airflow.hooks.postgres_hook import PostgresHook
 
 DEFAULT_ARGS = {
     'owner': 'Ida',
@@ -18,41 +23,35 @@ SQL_FOLDER = '/tmp/data/sql'
 FINAL_DATA_FOLDER = '/tmp/data/final_data'
 
 update_articles_in_DB_dag = DAG(
-    dag_id='update_articles_in_DB',
+    dag_id='update_articles_in_DB', 
     default_args=DEFAULT_ARGS,
-    start_date=datetime(2022, 10, 12, 14, 0, 0),
+    start_date=datetime(2022,10,12,14,0,0),
     schedule_interval=None,
-    # schedule_interval=@monthly,
+    #schedule_interval=@monthly,
     catchup=False,
     template_searchpath=[SQL_FOLDER]
 )
 
+def do_opencitation_call(DOI):
+    import opencitingpy
+    client = opencitingpy.client.Client()
+    try:
+        no_citations = client.get_citation_count(DOI)
+        return no_citations
+    except:
+        return -1
 
 def update_publications():
     import pandas as pd
-    import opencitingpy
-
-    client = opencitingpy.client.Client()
 
     conn = PostgresHook(postgres_conn_id='airflow_pg').get_conn()
-    publications_old_df = pd.read_sql('SELECT * FROM publications', conn)
-    publications_old_df_with_DOIS = publications_old_df[publications_old_df['doi'].notna()]
-
-    publications_new_df = pd.DataFrame(columns=['publication_ID', 'number_of_citations', 'updated_at'])
-    for i in range(len(publications_old_df_with_DOIS)):
-        article_DOI = publications_old_df_with_DOIS.iloc[i]['doi']
-        publication_ID = publications_old_df_with_DOIS.iloc[i]['publication_id']
-        old_no_citations = publications_old_df_with_DOIS.iloc[i]['number_of_citations']
-        try:
-            no_citations = client.get_citation_count(article_DOI)
-        except:
-            no_citations = old_no_citations
-        if no_citations != old_no_citations:
-            publications_new_df.loc[len(publications_new_df.index)] = [int(publication_ID), int(no_citations),
-                                                                       datetime.now(timezone.utc)]
-
+    publications_old_df = pd.read_sql('SELECT publication_id, doi, number_of_citations FROM publications WHERE doi IS NOT NULL', conn)
+    publications_old_df['number_of_citations_new'] = publications_old_df.apply(lambda x: do_opencitation_call(x.doi), axis=1)
+    publications_new_df = publications_old_df[publications_old_df.number_of_citations_new.gt(publications_old_df.number_of_citations)]
+    publications_new_df = publications_new_df.drop(['doi', 'number_of_citations'], axis=1)
+    publications_new_df = publications_new_df.rename(columns={'number_of_citations_new': 'number_of_citations'})
+    publications_new_df['updated_at'] = datetime.now(timezone.utc)
     publications_new_df.to_csv(f'{DATA2DB_FOLDER}/updated_publications_df.tsv', sep="\t", index=False)
-
 
 update_publications_data = PythonOperator(
     task_id='update_publications_data',
@@ -61,14 +60,13 @@ update_publications_data = PythonOperator(
 )
 
 truncate_updated_publications_table = PostgresOperator(
-    task_id='truncate_updated_publications_table',
+	task_id='truncate_updated_publications_table',
     dag=update_articles_in_DB_dag,
-    postgres_conn_id='airflow_pg',
-    sql="TRUNCATE updated_publications"
+	postgres_conn_id='airflow_pg',
+	sql="TRUNCATE updated_publications"
 )
 
-update_publications_data >> truncate_updated_publications_table
-
+update_publications_data >> truncate_updated_publications_table    
 
 def tsv_to_db(file_name, DB_table):
     get_postgres_conn = PostgresHook(postgres_conn_id='airflow_pg').get_conn()
@@ -77,7 +75,6 @@ def tsv_to_db(file_name, DB_table):
         next(f)
         curr.copy_from(f, DB_table, sep='\t')
         get_postgres_conn.commit()
-
 
 load_updated_publications_data = PythonOperator(
     task_id='load_updated_publications_data',
@@ -120,14 +117,12 @@ create_updated_venues_view = PostgresOperator(
     autocommit=True,
 )
 
-
 def copy_data_from_DB(output_folder, SQL_statement, data_type):
     import pandas as pd
     conn = PostgresHook(postgres_conn_id='airflow_pg').get_conn()
     df = pd.read_sql(SQL_statement, conn)
-    file_name = data_type + '_' + datetime.now().strftime("%Y%m%d%H%M%S") + '.csv'
+    file_name = data_type + '_' + datetime.now().strftime("%Y%m%d%H%M%S") + '.csv'    
     df.to_csv(f'{output_folder}/{file_name}', index=False)
-
 
 copy_updated_publications = PythonOperator(
     task_id='copy_updated_publications',
