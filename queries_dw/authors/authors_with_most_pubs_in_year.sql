b7ee3f2d7be97878122504f8c9df8324ce3abe8b
Siim Karel Koger <siimkkoger@gmail.com>
2023-01-15 17:24:05 +0100

    some more dwh query fixes

--- queries_dw/authors/authors_with_most_pubs_in_year.sql
+++ queries_dw/authors/authors_with_most_pubs_in_year.sql
@@ -12,6 +12,6 @@ JOIN warehouse.authors aut
 -- join time with publications
 JOIN warehouse.publication_time pub_time
 	ON pub.time_id = pub_time.id
-WHERE pub_time.year = '2023'
+WHERE pub_time.year = '2012'
 GROUP BY aut.id
 ORDER BY num_of_publications DESC;
