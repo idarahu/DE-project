e7e0547e2676b878830778c7902b1452aaf540c1
Siim Karel Koger <siimkkoger@gmail.com>
2023-01-15 09:23:42 +0100

    Fix dwh institution ranking queries

--- queries_dw/queries_ranking_institutions.sql
+++ queries_dw/queries_ranking_institutions.sql
@@ -2,112 +2,212 @@
 SELECT 
 	inst.name, 
 	COUNT(DISTINCT pub.doi) AS num_of_publications
-FROM 
-	warehouse.publications pub
-	JOIN warehouse.publication_institution pub_inst
-	JOIN warehouse.institution inst
-	JOIN warehouse.publication_time pub_time
+FROM warehouse.publications pub
+-- join institutions with publications
+JOIN warehouse.publication_institution pub_inst
+	ON pub_inst.publication_id = pub.id
+JOIN warehouse.institution inst
 	ON pub_inst.institution_id = inst.id
-	AND pub_inst.publication_id = pub.id
-	AND pub.publication_time = pub_time.id
-WHERE 
+-- join time with publications
+JOIN warehouse.publication_time pub_time
+	ON pub.time_id = pub_time.id
+WHERE
 	pub_time.year = '2023'
-GROUP BY aut.id
+GROUP BY inst.id
 ORDER BY num_of_publications DESC;
 
 -- Ranking institutions with the most publications in scientific domain
-SELECT 
-	inst.name, 
+SELECT
+	inst.name,
 	COUNT(DISTINCT pub.doi) AS num_of_publications
-FROM 
-	warehouse.publications pub
-	JOIN warehouse.publication_institution pub_inst
-	JOIN warehouse.institution inst
-	JOIN warehouse.publication_domain pub_domain
-	JOIN warehouse.scientific_domain scientific_domain
+FROM warehouse.publications pub
+-- join institutions with publications
+JOIN warehouse.publication_institution pub_inst
+	ON pub_inst.publication_id = pub.id
+JOIN warehouse.institution inst
 	ON pub_inst.institution_id = inst.id
-	AND pub_inst.publication_id = pub.id
-	AND pub_domain.domain_id = scientific_domain.id
-WHERE 
-	scientific_domain.id = 'scientific_domain_id'
-GROUP BY aut.id
+-- join scientific domains with publications
+JOIN warehouse.publication_domain pub_domain
+	ON pub_domain.publication_id = pub.id
+JOIN warehouse.scientific_domain scientific_domain
+	ON pub_domain.domain_id = scientific_domain.id
+WHERE
+	scientific_domain.id = 1
+GROUP BY inst.id
 ORDER BY num_of_publications DESC;
 
 -- Ranking institutions with the most publications in publication venue
 SELECT 
 	inst.name, 
 	COUNT(DISTINCT pub.doi) AS num_of_publications
-FROM 
-	warehouse.publications pub
-	JOIN warehouse.publication_institution pub_inst
-	JOIN warehouse.institution inst
-	JOIN warehouse.publication_venues pub_venues
+FROM warehouse.publications pub
+-- join institutions with publications
+JOIN warehouse.publication_institution pub_inst
+	ON pub_inst.publication_id = pub.id
+JOIN warehouse.institution inst
 	ON pub_inst.institution_id = inst.id
-	AND pub_inst.publication_id = pub.id
-	AND pub_venues.publication_id = pub.id
-WHERE 
-	pub_venues.id = 'pub_venues_id'
-GROUP BY aut.id
+-- join venues with publications
+JOIN warehouse.publication_venues pub_venues
+	ON pub_venues.id = pub.venue_id
+WHERE
+	pub_venues.id = 1
+GROUP BY inst.id
 ORDER BY num_of_publications DESC;
 
 -- Ranking institutions with the most publications in a given year, scientific domain, and in publication venue
 SELECT 
 	inst.name, 
 	COUNT(DISTINCT pub.doi) AS num_of_publications
-FROM 
-	warehouse.publications pub
-	JOIN warehouse.publication_institution pub_inst
-	JOIN warehouse.institution inst
-	JOIN warehouse.publication_time pub_time
-	JOIN warehouse.publication_domain pub_domain
-	JOIN warehouse.scientific_domain scientific_domain
-	JOIN warehouse.publication_venues pub_venues
+FROM warehouse.publications pub
+-- join institutions with publications
+JOIN warehouse.publication_institution pub_inst
+	ON pub_inst.publication_id = pub.id
+JOIN warehouse.institution inst
 	ON pub_inst.institution_id = inst.id
-	AND pub_inst.publication_id = pub.id
-	AND pub.publication_time = pub_time.id
-	AND pub_domain.domain_id = scientific_domain.id
-	AND pub_venues.publication_id = pub.id
-WHERE 
+-- join scientific domains with publications
+JOIN warehouse.publication_domain pub_domain
+	ON pub_domain.publication_id = pub.id
+JOIN warehouse.scientific_domain scientific_domain
+	ON pub_domain.domain_id = scientific_domain.id
+-- join venues with publications
+JOIN warehouse.publication_venues pub_venues
+	ON pub_venues.id = pub.venue_id
+-- join time with publications
+JOIN warehouse.publication_time pub_time
+	ON pub.time_id = pub_time.id
+WHERE
 	pub_time.year = '2023'
-	AND scientific_domain.id = 'scientific_domain_id'
-	AND pub_venues.id = 'pub_venues_id'
-GROUP BY aut.id
+	AND scientific_domain.id = 1
+	AND pub_venues.id = 1
+GROUP BY inst.id
 ORDER BY num_of_publications DESC;
 
+-- Ranking institutions with the most citations in a given year
+SELECT
+	inst.name,
+	SUM(pub.pub_num_of_citations) AS number_of_citations
+FROM
+	-- use latest DOIs
+	(
+		SELECT
+			DISTINCT ON (pu.doi)
+			pu.id AS pub_id,
+			pu.time_id AS pub_time_id,
+			pu.number_of_citations AS pub_num_of_citations,
+			pu.venue_id AS pub_venue_id
+		FROM warehouse.publications pu
+		ORDER BY pu.doi, pu.snapshot_valid_to DESC
+	) pub
+-- join institutions with publications
+JOIN warehouse.publication_institution pub_inst
+	ON pub_inst.publication_id = pub.pub_id
+JOIN warehouse.institution inst
+	ON pub_inst.institution_id = inst.id
+-- join time with publications
+JOIN warehouse.publication_time pub_time
+	ON pub.pub_time_id = pub_time.id
+WHERE
+	pub_time.year = '2023'
+GROUP BY inst.id
+ORDER BY number_of_citations DESC;
 
------------------------------------------------------------------------------------------------------------
+-- Ranking institutions with the most citations in a given scientific domain
+SELECT
+	inst.name,
+	SUM(pub.pub_num_of_citations) AS number_of_citations
+FROM
+	-- use latest DOIs
+	(
+		SELECT
+			DISTINCT ON (pu.doi)
+			pu.id AS pub_id,
+			pu.time_id AS pub_time_id,
+			pu.number_of_citations AS pub_num_of_citations,
+			pu.venue_id AS pub_venue_id
+		FROM warehouse.publications pu
+		ORDER BY pu.doi, pu.snapshot_valid_to DESC
+	) pub
+-- join institutions with publications
+JOIN warehouse.publication_institution pub_inst
+	ON pub_inst.publication_id = pub.pub_id
+JOIN warehouse.institution inst
+	ON pub_inst.institution_id = inst.id
+-- join scientific domains with publications
+JOIN warehouse.publication_domain pub_domain
+	ON pub_domain.publication_id = pub.pub_id
+JOIN warehouse.scientific_domain scientific_domain
+	ON pub_domain.domain_id = scientific_domain.id
+WHERE
+	scientific_domain.id = 1
+GROUP BY inst.id
+ORDER BY number_of_citations DESC;
 
+-- Ranking institutions with the most citations in a given publication venue
+SELECT
+	inst.name,
+	SUM(pub.pub_num_of_citations) AS number_of_citations
+FROM
+	-- use latest DOIs
+	(
+		SELECT
+			DISTINCT ON (pu.doi)
+			pu.id AS pub_id,
+			pu.time_id AS pub_time_id,
+			pu.number_of_citations AS pub_num_of_citations,
+			pu.venue_id AS pub_venue_id
+		FROM warehouse.publications pu
+		ORDER BY pu.doi, pu.snapshot_valid_to DESC
+	) pub
+-- join institutions with publications
+JOIN warehouse.publication_institution pub_inst
+	ON pub_inst.publication_id = pub.pub_id
+JOIN warehouse.institution inst
+	ON pub_inst.institution_id = inst.id
+-- join venues with publications
+JOIN warehouse.publication_venues pub_venues
+	ON pub_venues.id = pub.pub_venue_id
+WHERE
+	pub_venues.id = 1
+GROUP BY inst.id
+ORDER BY number_of_citations DESC;
 
 -- Ranking institutions with the most citations in a given year, scientific domain, and in publication venue
-SELECT 
+SELECT
 	inst.name, 
-	SUM(pub.number_of_citations) AS number_of_citations
-FROM 
+	SUM(pub.pub_num_of_citations) AS number_of_citations
+FROM
+	-- use latest DOIs
 	(
-		-- Only look at the latest DOI (multiple rows can have same DOI). 
-		-- Otherwise citations of same paper are counted multiple times.
-		SELECT 
+		SELECT
 			DISTINCT ON (pu.doi)
-			pu.id AS pub_id
+			pu.id AS pub_id,
+			pu.time_id AS pub_time_id,
+			pu.number_of_citations AS pub_num_of_citations,
+			pu.venue_id AS pub_venue_id
 		FROM warehouse.publications pu
-		ORDER BY pu.snapshot_valid_to DESC
+		ORDER BY pu.doi, pu.snapshot_valid_to DESC
 	) pub
-	JOIN warehouse.publication_institution pub_inst
-	JOIN warehouse.institution inst
-	JOIN warehouse.publication_time pub_time
-	JOIN warehouse.publication_domain pub_domain
-	JOIN warehouse.scientific_domain scientific_domain
-	JOIN warehouse.publication_venues pub_venues
+-- join institutions with publications
+JOIN warehouse.publication_institution pub_inst
+	ON pub_inst.publication_id = pub.pub_id
+JOIN warehouse.institution inst
 	ON pub_inst.institution_id = inst.id
-	AND pub_inst.publication_id = pub.id
-	AND pub.publication_time = pub_time.id
-	AND pub_domain.domain_id = scientific_domain.id
-	AND pub_venues.publication_id = pub.id
-WHERE 
+-- join scientific domains with publications
+JOIN warehouse.publication_domain pub_domain
+	ON pub_domain.publication_id = pub.pub_id
+JOIN warehouse.scientific_domain scientific_domain
+	ON pub_domain.domain_id = scientific_domain.id
+-- join venues with publications
+JOIN warehouse.publication_venues pub_venues
+	ON pub_venues.id = pub.pub_venue_id
+-- join time with publications
+JOIN warehouse.publication_time pub_time
+	ON pub.pub_time_id = pub_time.id
+WHERE
 	pub_time.year = '2023'
-	AND scientific_domain.id = 'scientific_domain_id'
-	AND pub_venues.id = 'pub_venues_id'
-GROUP BY aut.id
+	AND scientific_domain.id = 1
+	AND pub_venues.id = 1
+GROUP BY inst.id
 ORDER BY number_of_citations DESC;
 
 
