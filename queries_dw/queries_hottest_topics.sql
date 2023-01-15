41b6222d1ad4e326f1fa2fabb2f85459bc0a9988
Siim Karel Koger <siimkkoger@gmail.com>
2023-01-15 09:52:18 +0100

    Delete unneccesary file

--- queries_dw/queries_hottest_topics.sql
+++ queries_dw/queries_hottest_topics.sql
@@ -1,4 +1,7 @@
--- Ranking venues/journals with the highest h-index in a given time period
+-- What are the year's hottest topics (categories of scientific disciplines)?
+-- Hottest = most publications
+-- By venue, by institution
+
 SELECT pub_venues.full_name, pub_venues.h_index FROM (
 	SELECT 
 		DISTINCT ON (venue.full_name)
@@ -12,7 +15,35 @@ SELECT pub_venues.full_name, pub_venues.h_index FROM (
 ORDER BY pub_venues.h_index DESC;
 
 
-
+SELECT
+    COUNT()
+	scientific_domain.major_field,
+	scientific_domain.sub_category,
+	SUM(pub.number_of_citations) AS number_of_citations
+FROM
+	(
+		-- Only look at the latest DOI (multiple rows can have same DOI).
+		-- Otherwise citations of same paper are counted multiple times.
+		SELECT
+			DISTINCT ON (pu.doi)
+			pu.id AS pub_id,
+			pu.number_of_citations AS number_of_citations
+		FROM warehouse.publications pu
+		ORDER BY pu.snapshot_valid_to DESC
+	) pub
+	JOIN warehouse.publication_time pub_time
+	JOIN warehouse.publication_domain pub_domain
+	JOIN warehouse.scientific_domain scientific_domain
+	JOIN warehouse.publication_venues pub_venues
+	AND pub.publication_time = pub_time.id
+	AND pub_domain.domain_id = scientific_domain.id
+	AND pub_venues.publication_id = pub.id
+WHERE
+	pub_time.year = '2023'
+	AND scientific_domain.id = 'scientific_domain_id'
+	AND pub_venues.id = 'pub_venues_id'
+GROUP BY pub.doi
+ORDER BY number_of_citations DESC;
 
 
 
