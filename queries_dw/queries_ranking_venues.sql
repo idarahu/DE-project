b4da54c40454f93c87a66c2b522d1ca5fc1570e7
Siim Karel Koger <siimkkoger@gmail.com>
2023-01-15 09:51:23 +0100

    Fix dwh queries ranking venues

--- queries_dw/queries_ranking_venues.sql
+++ queries_dw/queries_ranking_venues.sql
@@ -1,15 +1,14 @@
--- Ranking venues/journals with the highest h-index in a given time period
-SELECT pub_venues.full_name, pub_venues.h_index FROM (
-	SELECT 
-		DISTINCT ON (venue.full_name)
+-- Ranking venues/journals with the highest h-index (calculated) in a given time period
+SELECT pub_venues.full_name, pub_venues.h_index_calculated FROM (
+	SELECT DISTINCT ON (venue.full_name)
 		venue.full_name AS full_name,
-		venue.h_index_calculated AS h_index
+		venue.h_index_calculated AS h_index_calculated
 	FROM warehouse.publication_venues venue
 	WHERE
 	    (venue.valid_from, venue.valid_to) OVERLAPS ('2012-01-01'::DATE, '2012-04-12'::DATE)
-	ORDER BY venue.valid_to DESC
+	ORDER BY venue.full_name, venue.valid_to DESC
 ) pub_venues
-ORDER BY pub_venues.h_index DESC;
+ORDER BY pub_venues.h_index_calculated DESC;
 
 
 
